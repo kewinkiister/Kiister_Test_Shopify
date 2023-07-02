26992f7eb54237aecaf71a279496db881b06e2b4
Kewin Kuster <kevinkiister0@gmail.com>
2023-07-02 12:30:15 -0300

    primeito commit

---
+++ tailwind.config.js
@@ -0,0 +1,14 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./layout/*.liquid', 
+  './sections/*.liquid',
+  './snippets/*.liquid',
+  './templates/custumers/*.liquid',
+  './templates/*.liquid'
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+
