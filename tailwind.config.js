634d00572325a9b1d78c1e58d54d8ea38037e1fb
Kewin Kuster <kevinkiister0@gmail.com>
2023-07-19 07:04:55 -0300

    finalização do projeto

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,12 @@
-/** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ['./layout/*.liquid', 
-  './sections/*.liquid',
-  './snippets/*.liquid',
-  './templates/custumers/*.liquid',
-  './templates/*.liquid'
+  content: [
+    './layout/*.liquid',
+    './sections/*.liquid',
+    './snippets/*.liquid',
+    './templates/*.liquid'
   ],
   theme: {
-    extend: {
-      height:{
-        '94':'22rem'
-      }
-    },
+    extend: {},
   },
   plugins: [],
 }
-
